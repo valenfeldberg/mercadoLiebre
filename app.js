78b6eecd3d783ee1b5ed374029e5cadaa4ee94a0
Valentina Feldberg <valentinafeldberg@Valentinas-MacBook-Pro.local>
2021-11-15 23:31:39 -0300

    formularios

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/views/home.html"))
 })
 
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/register.html"))
+})
+
+app.post('/', (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/home.html"))
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/login.html"))
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
